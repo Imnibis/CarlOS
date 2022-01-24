8c0fcd40cbb89944a626ba33aeea27507bc1f365
Rayane <imnibis@outlook.fr>
2022-01-24 01:17:00 +0100

    Added vote channel select

--- commands/democracy/CommandDemocracyEnable.ts
+++ commands/democracy/CommandDemocracyEnable.ts
@@ -1,4 +1,4 @@
-import { Client, CommandInteraction, MessageEmbed } from "discord.js";
+import { Client, CommandInteraction, Message, MessageActionRow, MessageEmbed, MessageSelectMenu, SelectMenuInteraction, TextChannel } from "discord.js";
 import Guild from "../../models/guild.model";
 import Command from "../../util/command";
 
@@ -18,6 +18,46 @@ export default class CommandDemocracyEnable extends Command
             .setTitle("Succès")
             .setDescription("Démocratie activée!")
             .setFooter(client.user.username, client.user.avatarURL());
-        interaction.reply({embeds: [embed]})
+        const row = new MessageActionRow().addComponents(
+            new MessageSelectMenu()
+                .setCustomId('selectChannel')
+                .setPlaceholder('Sélectionnez le channel de vote')
+                .addOptions(interaction.guild.channels.cache.filter(channel => channel.isText()).map(channel => (
+                    {
+                        label: `#${channel.name}`,
+                        value: channel.id,
+                    }
+                )))
+        )
+        interaction.reply({embeds: [embed], components: [row]});
+        const msg = await interaction.fetchReply() as Message;
+        const collector = msg.createMessageComponentCollector({componentType: 'SELECT_MENU'});
+
+        collector.on('collect', async (inter: SelectMenuInteraction) => {
+            if (inter.user.id !== interaction.user.id) {
+                const embed = new MessageEmbed()
+                    .setColor("#ff0000")
+                    .setTitle("Erreur")
+                    .setDescription("Seul l'utilisateur ayant activé la démocratie peut choisir le channel.")
+                    .setFooter(client.user.username, client.user.avatarURL());
+                inter.reply({embeds:[embed], ephemeral: true});
+                return;
+            }
+            await guild.setSetting('voteChannel', inter.values[0]);
+            const embed = new MessageEmbed()
+                .setColor("#00bfff")
+                .setTitle("Succès")
+                .setDescription("Channel défini !")
+                .setFooter(client.user.username, client.user.avatarURL());
+            inter.reply({embeds: [embed], ephemeral: true});
+            inter.guild.channels.fetch(inter.values[0]).then((channel: TextChannel) => {
+                const embed = new MessageEmbed()
+                    .setColor("#00bfff")
+                    .setTitle("Information")
+                    .setDescription("Ce channel a été défini comme channel de vote démocratique.")
+                    .setFooter(client.user.username, client.user.avatarURL());
+                channel.send({embeds: [embed]});
+            })
+        });
     }
 }
