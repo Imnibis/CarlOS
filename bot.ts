0f3d791a659c01bcbe77755a52a673dbbdb91aa0
Rayane <imnibis@outlook.fr>
2022-02-15 12:08:56 +0100

    Added intents

--- bot.ts
+++ bot.ts
@@ -12,8 +12,13 @@ class Bot
     constructor(token)
     {
         Bot.client = new Client({intents: [
-            Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES,
-            Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGES]});
+            Intents.FLAGS.GUILDS,
+            Intents.FLAGS.GUILD_MEMBERS,
+            Intents.FLAGS.GUILD_BANS,
+            Intents.FLAGS.GUILD_VOICE_STATES,
+            Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+            Intents.FLAGS.GUILD_MESSAGES,
+        ]});
         Bot.client.on("ready", this.onReady.bind(this));
         
         Bot.client.login(token);
