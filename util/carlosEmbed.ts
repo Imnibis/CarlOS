b5519b2406d6e09779dcfd3cdbe1f97e211175f3
Muhammad Rafat Kurkar <84540534+KMKINGMAN@users.noreply.github.com>
2022-08-24 15:36:52 +0300

    fix carlosEmbed and musicplayer

--- util/carlosEmbed.ts
+++ util/carlosEmbed.ts
@@ -7,21 +7,18 @@ export default class CarlOSEmbed extends EmbedBuilder
     constructor(color: number | RGBTuple) {
         super()
         this.setColor(color)
-            .setFooter({text: Bot.client.user.username, iconURL: Bot.client.user.avatarURL()});
+        this.setFooter({text: Bot.client.user.username, iconURL: Bot.client.user.avatarURL()});
     }
-
     static errorEmbed(error: string): CarlOSEmbed
     {
         return new CarlOSEmbed(Colors.Red)
             .setTitle("Erreur")
             .setDescription(error);
     }
-
     static infoEmbed(): CarlOSEmbed
     {
         return new CarlOSEmbed(Colors.Aqua)
     }
-
     static successEmbed(text: string): CarlOSEmbed
     {
         return new CarlOSEmbed(Colors.Green)
